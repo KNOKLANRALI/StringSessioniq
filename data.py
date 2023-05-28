faebd995909b8fce20b68ae9595b8add2071a245
KNOKLANRALI <113829965+KNOKLANRALI@users.noreply.github.com>
2023-05-28 04:13:34 +0300

    Update data.py

--- data.py
+++ data.py
@@ -2,61 +2,29 @@ from pyrogram.types import InlineKeyboardButton
 
 
 class Data:
-    generate_single_button = [InlineKeyboardButton("🔥 Start Generating Session 🔥", callback_data="generate")]
+    generate_single_button = [InlineKeyboardButton("🔥 اضغط هنا للاستخراج 🔥", callback_data="generate")]
 
     home_buttons = [
         generate_single_button,
-        [InlineKeyboardButton(text="🏠 Return Home 🏠", callback_data="home")]
+        [InlineKeyboardButton(text="🏠  رجوع 🏠", callback_data="home")]
     ]
 
     generate_button = [generate_single_button]
 
     buttons = [
         generate_single_button,
-        [InlineKeyboardButton("✨ Bot Status and More Bots ✨", url="https://t.me/StarkBots/7")],
-        [
-            InlineKeyboardButton("How to Use ❔", callback_data="help"),
-            InlineKeyboardButton("🎪 About 🎪", callback_data="about")
-        ],
-        [InlineKeyboardButton("♥ More Amazing bots ♥", url="https://t.me/StarkBots")],
+        [InlineKeyboardButton("✨ قناتنا ✨", url="https://t.me/iqthon")],
     ]
 
     START = """
 Hey {}
 
-Welcome to {}
+اهلا بك {}
 
-If you don't trust this bot, 
-1) stop reading this message
-2) delete this chat
+بوت ستخراج كود تيرمكس و كود بايروجرام
 
-Still reading?
-You can use me to generate pyrogram (even version 2) and telethon string session. Use below buttons to learn more !
-
-By @StarkBots
+By @iqthon
+شرح استخدام هنا @YZZZY
     """
 
-    HELP = """
-✨ **Available Commands** ✨
-
-/about - About The Bot
-/help - This Message
-/start - Start the Bot
-/generate - Generate Session
-/cancel - Cancel the process
-/restart - Cancel the process
-"""
-
-    ABOUT = """
-**About This Bot** 
-
-Telegram Bot to generate Pyrogram and Telethon string session by @StarkBots
-
-Source Code : [Click Here](https://github.com/StarkBotsIndustries/StringSessionBot)
-
-Framework : [Pyrogram](https://docs.pyrogram.org)
-
-Language : [Python](https://www.python.org)
-
-Developer : @StarkAgent
-    """
+    
